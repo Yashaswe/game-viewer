d09c81f2fb0a7b47ef8b1eb859ec3d74d3902117
Yashaswe <amatyayasu@gmail.com>
2022-12-26 20:05:33 -0500

    make react project and add assets

---
+++ src/assets/Cards/cards/index.ts
@@ -0,0 +1,107 @@
+import oneS from './1S.png';
+import oneH from './1H.png';
+import oneC from './1C.png';
+import oneD from './1D.png';
+import twoS from './2S.png';
+import twoH from './2H.png';
+import twoC from './2C.png';
+import twoD from './2D.png';
+import threeS from './3S.png';
+import threeH from './3H.png';
+import threeC from './3C.png';
+import threeD from './3D.png';
+import fourS from './4S.png';
+import fourH from './4H.png';
+import fourC from './4C.png';
+import fourD from './4D.png';
+import fiveS from './5S.png';
+import fiveH from './5H.png';
+import fiveC from './5C.png';
+import fiveD from './5D.png';
+import sixS from './6S.png';
+import sixH from './6H.png';
+import sixC from './6C.png';
+import sixD from './6D.png';
+import sevenS from './7S.png';
+import sevenH from './7H.png';
+import sevenC from './7C.png';
+import sevenD from './7D.png';
+import eightS from './8S.png';
+import eightH from './8H.png';
+import eightC from './8C.png';
+import eightD from './8D.png';
+import nineS from './9S.png';
+import nineH from './9H.png';
+import nineC from './9C.png';
+import nineD from './9D.png';
+import TS from './TS.png';
+import TH from './TH.png';
+import TC from './TC.png';
+import TD from './TD.png';
+import JS from './JS.png';
+import JH from './JH.png';
+import JC from './JC.png';
+import JD from './JD.png';
+import QS from './QS.png';
+import QH from './QH.png';
+import QC from './QC.png';
+import QD from './QD.png';
+import KS from './KS.png';
+import KH from './KH.png';
+import KC from './KC.png';
+import KD from './KD.png';
+
+export const cards = {
+  '1S': oneS,
+  '1H': oneH,
+  '1C': oneC,
+  '1D': oneD,
+  '2S': twoS,
+  '2H': twoH,
+  '2C': twoC,
+  '2D': twoD,
+  '3S': threeS,
+  '3H': threeH,
+  '3C': threeC,
+  '3D': threeD,
+  '4S': fourS,
+  '4H': fourH,
+  '4C': fourC,
+  '4D': fourD,
+  '5S': fiveS,
+  '5H': fiveH,
+  '5C': fiveC,
+  '5D': fiveD,
+  '6S': sixS,
+  '6H': sixH,
+  '6C': sixC,
+  '6D': sixD,
+  '7S': sevenS,
+  '7H': sevenH,
+  '7C': sevenC,
+  '7D': sevenD,
+  '8S': eightS,
+  '8H': eightH,
+  '8C': eightC,
+  '8D': eightD,
+  '9S': nineS,
+  '9H': nineH,
+  '9C': nineC,
+  '9D': nineD,
+  TS: TS,
+  TH: TH,
+  TC: TC,
+  TD: TD,
+  JS: JS,
+  JH: JH,
+  JC: JC,
+  JD: JD,
+  QS: QS,
+  QH: QH,
+  QC: QC,
+  QD: QD,
+  KS: KS,
+  KH: KH,
+  KC: KC,
+  KD: KD,
+};
